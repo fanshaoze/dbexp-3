ed659c680569d7c4535357b8f1499451aae09332
fanshaoze <fanshaoze123123@163.com>
2016-06-08 13:16:32 +0800

    好像是改了什么，忘了

--- dbexp3-v1/sort.h
+++ dbexp3-v1/sort.h
@@ -13,3 +13,4 @@ int sortinit();//把R的各块分别排序，形成初始归并段
 int mergeSort(unsigned int addr1, int n1, unsigned int addr2, int n2, int base);
 int mergeAll();
 int binarysort(int array[],int val);
+int hashsort(int array[], int val);
